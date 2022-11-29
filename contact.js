65b5cbb82e157fe9bcb38fb104b66e9b64d4a94e
TineoC <christophertineo02@gmail.com>
2022-11-29 18:37:37 -0400

    add contact, thanks page

---
+++ contact.js
@@ -0,0 +1,13 @@
+import countryList from "./countries.json" assert { type: "json" };
+
+let select = document.getElementById("country");
+
+countryList.map((country) => {
+	const { name: countryName, code: countryCode } = country;
+
+	let countryOption = document.createElement("option");
+	countryOption.value = countryCode;
+	countryOption.innerHTML = countryName;
+
+	select.appendChild(countryOption);
+});
